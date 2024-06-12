b929a77b473486bef4d9739931de225b86bd1e47
username <useremail>
2024-06-11 21:18:43 -0700

    Starting the virtual assistant opener.

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,1 +1,6 @@
-print("Hello World")
+print("Hello, my name is Alex your virutal assistant. I will help you order a pizza!")
+print("I am going to ask you a few questions. After each response please press Enter.")
+userName = input("\nWhat is your name?:  ")
+print(f"\nNice to meet you, {userName}!")
+
+
